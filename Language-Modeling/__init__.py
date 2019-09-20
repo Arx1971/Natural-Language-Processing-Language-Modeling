ebabc92f83f4707d0b6abd2b0b1ba1460e506cd1
arx1971 <adnan.rahin13@gmail.com>
2019-09-20 19:48:34 -0400

    Punctuation Removed: Data Process

--- Language-Modeling/__init__.py
+++ Language-Modeling/__init__.py
@@ -1,8 +1,11 @@
 import math
 import re
+import string
+from string import punctuation
 
 
 # Section 1.1 Pre-Processing the data set
+
 # Pad each sentence
 def padding_model(file_path):
     with open(file_path, "r") as lines:
@@ -28,14 +31,25 @@ def lower_case_all_words(data_set):
 def replace_to_unk(data_set):
     duplicates = set()
     modified_data_set = []
-    for i in range(0, 10):
-        str = re.split("\s+", data_set[i])
-        for j in range(1, len(str) - 1):
-            if str[j] in duplicates and str:
-                str[j] = "<unk>"
+    for i in range(0, len(data_set)):
+
+        sentence = data_set[i].strip()
+
+        words = sentence.split()
+
+        table = str.maketrans('', '', string.punctuation)
+
+        stripped = [words[j].translate(table) for j in range(1, len(words) - 1)]
+
+        for j in range(0, len(stripped)):
+            if stripped[j] in duplicates:
+                stripped[j] = "<unk>"
             else:
-                duplicates.add(str[j])
-        modified_data_set.append(str)
+                duplicates.add(stripped[j])
+        stripped.insert(0, "<s>")
+        stripped.append("</s>")
+        modified_data_set.append(stripped)
+
     return modified_data_set
 
 
@@ -50,8 +64,17 @@ brown_test_data = padding_model(brown_test_path)
 brown_train_data = padding_model(brown_train_path)
 
 brown_test_data = lower_case_all_words(brown_test_data)
+
 brown_train_data = lower_case_all_words(brown_train_data)
+
 new_data_set = replace_to_unk(brown_train_data)
 
 for i in range(0, len(new_data_set)):
     print(new_data_set[i])
+# v = "my name is megatron's."
+#
+# words = v.split()
+#
+# table = str.maketrans('', '', string.punctuation)
+# stripped = [w.translate(table) for w in words]
+# print(stripped)
