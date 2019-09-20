d2ed0617304829a0ce3c02039c718234512557ea
arx1971 <adnan.rahin13@gmail.com>
2019-09-20 02:14:14 -0400

    "Def padding Method added"

--- Language-Modeling/__init__.py
+++ Language-Modeling/__init__.py
@@ -1,1 +1,20 @@
-print("test")
+import math
+import re
+
+
+# Pad each sentence
+def padding_model(flie_path):
+    with open(flie_path, "r") as lines:
+        str_array = []
+        for line in lines:
+            line = line[0:len(line) - 1]
+            str_array.append("<s> " + line + " </s>")
+
+    return str_array
+
+
+browntest = "/home/adnanrahin/source-code/PycharmProjects/Natural-Language-Processing-Language-Modeling/Data-Set/brown-test.txt"
+
+str = padding_model(browntest)
+for i in range(0, len(str)):
+    print(str[i])
