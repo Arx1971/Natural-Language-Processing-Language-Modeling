cd2f3c060c58bfadb9a3f70b88db8b4391df8e30
arx1971 <adnan.rahin13@gmail.com>
2019-09-20 15:13:09 -0400

    "Pre-Processing Data Set"

--- Language-Modeling/__init__.py
+++ Language-Modeling/__init__.py
@@ -2,6 +2,7 @@ import math
 import re
 
 
+# Section 1.1 Pre-Processing the data set
 # Pad each sentence
 def padding_model(flie_path):
     with open(flie_path, "r") as lines:
@@ -13,9 +14,24 @@ def padding_model(flie_path):
     return str_array
 
 
-brown_test_path = "/home/adnanrahin/source-code/PycharmProjects/Natural-Language-Processing-Language-Modeling/Data-Set/brown-test.txt"
-brown_train_path = "/home/adnanrahin/source-code/PycharmProjects/Natural-Language-Processing-Language-Modeling/Data-Set/brown-train.txt"
+# LowerCase all words
+
+def lower_case_all_words(data_set):
+    for i in range(0, len(data_set)):
+        data_set[i] = data_set[i].lower()
+
+    return data_set
+
+
+# main method
+
+path = "/home/adnanrahin/source-code/PycharmProjects/Natural-Language-Processing-Language-Modeling"
+
+brown_test_path = path + "/Data-Set/brown-test.txt"
+brown_train_path = path + "/Data-Set/brown-train.txt"
 
 brown_test_data = padding_model(brown_test_path)
 brown_train_data = padding_model(brown_train_path)
 
+brown_test_data = lower_case_all_words(brown_test_data)
+brown_train_data = lower_case_all_words(brown_train_data)
