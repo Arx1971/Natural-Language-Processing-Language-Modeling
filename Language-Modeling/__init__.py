43c389a9f557d48de5bcc11910a12903d6579433
arx1971 <adnan.rahin13@gmail.com>
2019-09-20 17:54:21 -0400

    Duplicates Words Dumped

--- Language-Modeling/__init__.py
+++ Language-Modeling/__init__.py
@@ -4,8 +4,8 @@ import re
 
 # Section 1.1 Pre-Processing the data set
 # Pad each sentence
-def padding_model(flie_path):
-    with open(flie_path, "r") as lines:
+def padding_model(file_path):
+    with open(file_path, "r") as lines:
         str_array = []
         for line in lines:
             line = line[0:len(line) - 1]
@@ -23,6 +23,22 @@ def lower_case_all_words(data_set):
     return data_set
 
 
+# Replace all words occurring in the training data once with the token <unk>
+
+def replace_to_unk(data_set):
+    duplicates = set()
+    modified_data_set = []
+    for i in range(0, 10):
+        str = re.split("\s+", data_set[i])
+        for j in range(1, len(str) - 1):
+            if str[j] in duplicates and str:
+                str[j] = "<unk>"
+            else:
+                duplicates.add(str[j])
+        modified_data_set.append(str)
+    return modified_data_set
+
+
 # main method
 
 path = "/home/adnanrahin/source-code/PycharmProjects/Natural-Language-Processing-Language-Modeling"
@@ -35,3 +51,7 @@ brown_train_data = padding_model(brown_train_path)
 
 brown_test_data = lower_case_all_words(brown_test_data)
 brown_train_data = lower_case_all_words(brown_train_data)
+new_data_set = replace_to_unk(brown_train_data)
+
+for i in range(0, len(new_data_set)):
+    print(new_data_set[i])
