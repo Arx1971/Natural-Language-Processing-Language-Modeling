429687d3309ca1abed471bc9ceee8699e5dd9dc0
Arx1971 <adnan.rahin13@gmail.com>
2019-09-22 19:03:11 -0400

    Test Data filtered

--- Language-Modeling/__init__.py
+++ Language-Modeling/__init__.py
@@ -9,12 +9,12 @@ def load_data_set(filename):
         str_array = []
         for line in lines:
             line = line[0:len(line) - 1]
-            str_array.append("<s> "+ line.lower() + " </s>\n")
+            str_array.append("<s> " + line.lower() + " </s>\n")
 
     return str_array
 
 
-def data_process(data_set, filename):
+def training_data_process(data_set, filename):
     file = open("updated-" + filename, "w")
     dictionary = dict()
     pattern = "[A-Za-z]"
@@ -33,8 +33,29 @@ def data_process(data_set, filename):
             if re.match(pattern, word):
                 if dictionary[word] == 1:
                     file.write(" <unk>")
+                    del dictionary[word]
                 else:
                     file.write(" " + word)
+            else:
+                file.write(" " + word)
+        file.write("\n")
+
+    file.close()
+
+    return dictionary
+
+
+def test_data_process(dictionary, dataset, filename):
+    file = open("updated-" + filename, "w")
+    pattern = "[A-Za-z]"
+    for sentence in dataset:
+        words = sentence.split()
+        for word in words:
+            if re.match(pattern, word):
+                if word in dictionary:
+                    file.write(" " + word)
+                else:
+                    file.write(" " + "<unk>")
             else:
                 file.write(" " + word)
         file.write("\n")
@@ -42,4 +63,11 @@ def data_process(data_set, filename):
 
 
 training_data_set = load_data_set("brown-train.txt")
-data_process(training_data_set, "brown-train.txt")
+test_data_set = load_data_set("brown-test.txt")
+
+dictionary = training_data_process(training_data_set, "brown-train.txt")
+test_data_process(dictionary, test_data_set, "brown-test.txt")
+# for word, count in dictionary.items():
+#     print(word, count)
+
+print(len(dictionary) + 3)
