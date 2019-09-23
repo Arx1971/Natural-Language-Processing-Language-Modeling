f6b74e9f32913833fb44ff2cd88ad8acfce0955f
adnanrahin <adnan.rahin13@gmail.com>
2019-09-23 16:41:29 -0400

    Word Token

--- Language-Modeling/__init__.py
+++ Language-Modeling/__init__.py
@@ -17,25 +17,20 @@ def load_data_set(filename):
 def training_data_process(data_set, filename):
     file = open("updated-" + filename, "w")
     dictionary = dict()
-    pattern = "[A-Za-z]"
     for sentence in data_set:
         words = sentence.split()
         for word in words:
-            if re.match(pattern, word):
-                if word not in dictionary:
-                    dictionary[word] = 1
-                else:
-                    dictionary[word] += 1
+            if word not in dictionary:
+                dictionary[word] = 1
+            else:
+                dictionary[word] += 1
 
     for sentence in data_set:
         words = sentence.split()
         for word in words:
-            if re.match(pattern, word):
-                if dictionary[word] == 1:
-                    file.write(" <unk>")
-                    del dictionary[word]
-                else:
-                    file.write(" " + word)
+            if dictionary[word] == 1:
+                file.write(" <unk>")
+                del dictionary[word]
             else:
                 file.write(" " + word)
         file.write("\n")
@@ -47,17 +42,13 @@ def training_data_process(data_set, filename):
 
 def test_data_process(dictionary, dataset, filename):
     file = open("updated-" + filename, "w")
-    pattern = "[A-Za-z]"
     for sentence in dataset:
         words = sentence.split()
         for word in words:
-            if re.match(pattern, word):
-                if word in dictionary:
-                    file.write(" " + word)
-                else:
-                    file.write(" " + "<unk>")
-            else:
+            if word in dictionary:
                 file.write(" " + word)
+            else:
+                file.write(" " + "<unk>")
         file.write("\n")
     file.close()
 
@@ -67,7 +58,5 @@ test_data_set = load_data_set("brown-test.txt")
 
 dictionary = training_data_process(training_data_set, "brown-train.txt")
 test_data_process(dictionary, test_data_set, "brown-test.txt")
-# for word, count in dictionary.items():
-#     print(word, count)
 
-print(len(dictionary) + 3)
+print("Total Number of Unique Words in Training Corpus: ", len(dictionary) + 3)
