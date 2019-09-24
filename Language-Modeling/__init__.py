f4fb3fd36c71a5f3575ec21978c143a131434583
Arx1971 <adnan.rahin13@gmail.com>
2019-09-24 02:49:11 -0400

    Error 102: Token and Words Types

--- Language-Modeling/__init__.py
+++ Language-Modeling/__init__.py
@@ -61,10 +61,37 @@ def test_data_process(dictionary, dataset, filename):
     file.close()
 
 
+def percentage_word_and_token(test_data, train_data):
+    train_data_map = dict()
+    test_data_map = dict()
+    test_token_counter = 0
+    train_token_counter = 0
+
+    for sentence in train_data:
+        words = sentence.split()
+        for word in words:
+            train_token_counter += 1
+            if word not in train_data_map:
+                train_data_map[word] = 1
+            else:
+                train_data_map[word] += 1
+
+    for sentence in test_data:
+        words = sentence.split()
+        for word in words:
+            test_token_counter += 1
+            if word not in test_data_map:
+                test_data_map[word] = 1
+            else:
+                test_data_map[word] += 1
+
+    print(len(train_data_map), " ", train_token_counter)
+    print(len(test_data_map), " ", test_token_counter)
+
+
 training_data_set = load_data_set("brown-train.txt")
 test_data_set = load_data_set("brown-test.txt")
-
 dictionary = training_data_process(training_data_set, "brown-train.txt")
 test_data_process(dictionary, test_data_set, "brown-test.txt")
 
-print(dictionary['<unk>'])
+percentage_word_and_token(test_data_set, training_data_set)
