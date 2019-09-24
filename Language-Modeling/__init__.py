58fb9525e29aba9357dd2dfc85d4f7e0d85c4a6a
Arx1971 <adnan.rahin13@gmail.com>
2019-09-24 02:23:54 -0400

    Bug Fixed

--- Language-Modeling/__init__.py
+++ Language-Modeling/__init__.py
@@ -27,19 +27,22 @@ def training_data_process(data_set, filename):
             else:
                 dictionary[word] += 1
 
+    dictionary["<unk>"] = 1
+
     for sentence in data_set:
         words = sentence.split()
         for word in words:
             if dictionary[word] == 1:
                 file.write(" <unk>")
                 del dictionary[word]
+                dictionary["<unk>"] += 1
             else:
                 file.write(" " + word)
         file.write("\n")
 
     file.close()
 
-    print("Total Number of Unique Words in Training Corpus: ", len(dictionary) + 3)
+    print("Total Number of Unique Words in Training Corpus: ", len(dictionary))
     print("Total Number of Token in Training Corpus: ", token_counter)
 
     return dictionary
@@ -63,3 +66,5 @@ test_data_set = load_data_set("brown-test.txt")
 
 dictionary = training_data_process(training_data_set, "brown-train.txt")
 test_data_process(dictionary, test_data_set, "brown-test.txt")
+
+print(dictionary['<unk>'])
